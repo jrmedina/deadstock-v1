eea14d147150c0d654cc13a072903d8ffb5c0a93
jmedina <ndgns2@gmail.com>
2022-12-09 11:46:15 -0700

    Switch servers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,7 @@
 const fetchData = async (type) => {
   try {
     //https://deadstock-api.vercel.app/
-    const response = await fetch(
-      `https://deadstock-api.vercel.app/api/${type}`
-    );
+    const response = await fetch(` http://localhost:3001/api/${type}`);
     const json = await response.json();
     return json;
   } catch (error) {
