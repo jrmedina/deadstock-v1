bb52894833e3c60e4f2fc81cee80d7cdd9604ab1
jmedina <ndgns2@gmail.com>
2022-12-10 18:18:36 -0700

    deploy mobile

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -2,8 +2,11 @@ import { formatData } from "../utils/formatData";
 
 const fetchData = async (type) => {
   try {
-    //https://deadstock-api.vercel.app/
-    const response = await fetch(` http://localhost:3001/api/${type}`);
+    // ` http://localhost:3001/api/${type}`
+    // `https://deadstock-api.vercel.app//api/${type}`
+    const response = await fetch(
+      `https://deadstock-api.vercel.app/api/${type}`
+    );
     const json = await response.json();
     const data =
       type === "inventory" ? json.data.map((data) => formatData(data)) : json;
